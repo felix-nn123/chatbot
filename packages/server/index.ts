0125524a3384897c60227857649a3ef01ddcb331
felix-nn123 <upwork_fn@yahoo.com>
2026-03-22 06:57:59 +0100

    run both app together

---
+++ packages/server/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import type {Request, Response} from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const port=process.env.PORT || 3000;
+
+app.get('/api', (req: Request, res: Response)=>{
+    res.send('Hello World!!')
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is listening on http://localhost:${port}`)
+})
